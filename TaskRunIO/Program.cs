d5ade402adaf7a3b5e84126dcda8f95edfe9f88d
Pavel Levchuk <SpaierInstinct@gmail.com>
2019-01-15 00:50:25 +0300

    Add project files.

---
+++ TaskRunIO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TaskRunIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
