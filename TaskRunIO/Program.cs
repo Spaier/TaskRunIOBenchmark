0a415d604e84e183aa8fb2f0eb2e9fa33a1ba34f
Pavel Levchuk <SpaierInstinct@gmail.com>
2019-01-15 01:50:53 +0300

    feat: add task run io benchmark

--- TaskRunIO/Program.cs
+++ TaskRunIO/Program.cs
@@ -1,12 +1,76 @@
 ﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
 
 namespace TaskRunIO
 {
-    class Program
+    //[ClrJob, CoreJob, CoreRtJob, SimpleJob, VeryLongRunJob]
+    //[LongRunJob]
+    public class TaskRunIOBenchmark
     {
-        static void Main(string[] args)
+        public const string ImageName = @"../../../IMG_20180905_220918";
+
+        public const string ImageExt = ".jpg";
+
+        public const string ImagePath = ImageName + ImageExt;
+
+        public const int Amount = 4;
+
+        private Task GetTask(Func<Task> action) => Task.WhenAll
+            (
+                Enumerable
+                    .Repeat(action, Amount)
+                    .Select(it => action())
+                    .ToArray()
+            );
+
+        [Benchmark]
+        public Task AsyncAwaitCopyDelete()
         {
-            Console.WriteLine("Hello World!");
+            return GetTask(async () =>
+            {
+                var name = ImageName + Guid.NewGuid() + ImageExt;
+                var image = await Task.Run(() => new Bitmap(ImagePath));
+                await Task.Run(() => image.Save(name));
+                await Task.Run(() => File.Delete(name));
+            });
+        }
+
+        [Benchmark]
+        public Task AsyncAwaitFalseCopyDelete()
+        {
+            return GetTask(async () =>
+            {
+                var name = ImageName + Guid.NewGuid() + ImageExt;
+                var image = await Task.Run(() => new Bitmap(ImagePath)).ConfigureAwait(false);
+                await Task.Run(() => image.Save(name)).ConfigureAwait(false);
+                await Task.Run(() => File.Delete(name)).ConfigureAwait(false);
+            });
+        }
+
+        [Benchmark]
+        public Task SyncCopyDelete()
+        {
+            return GetTask(async () =>
+            {
+                var name = ImageName + Guid.NewGuid() + ImageExt;
+                var image = new Bitmap(ImagePath);
+                image.Save(name);
+                File.Delete(name);
+            });
+        }
+    }
+
+    static class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<TaskRunIOBenchmark>();
+            Console.ReadKey();
         }
     }
 }
